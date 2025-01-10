683c73ea4bdf41f0ca46dcb57480fe438c90a216
ganesh.balasaheb <ganesh.balasaheb@maqta.ae>
2025-01-10 14:43:22 +0400

    Updates

--- src/components/admin/admin.js
+++
@@ -1,8 +0,0 @@
-import './admin.css'
-const Admin = () => {
-    return <div className="Admin">
-        Admin
-    </div>;
-};
-
-export default Admin;
--- src/components/store/actions/axios-setup.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  //baseURL: "http://localhost:8080/",
-  baseURL: "https://noteapi-7xnh.onrender.com",
-});
-
-export default instance;
